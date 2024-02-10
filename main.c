9231db84451fafe504d84fd22681665e33556e97
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2024-02-10 16:51:36 +0800

    can close and free all memory. todo: resize

--- main.c
+++ main.c
@@ -24,11 +24,11 @@ typedef struct t_params
 
 } s_params;
 
-int close_window(void *params)
+int close_window(s_params *p)
 {
-	s_params *p = (s_params *) params;
-
 	mlx_destroy_window(p->mlx_ptr, p->win_ptr);
+	mlx_destroy_display(p->mlx_ptr);
+	free(p->mlx_ptr);
 	exit(1);
 	return (1);
 }
@@ -36,18 +36,15 @@ int close_window(void *params)
 int press(int keycode, s_params *p)
 {
 	printf("Key: %i\n", keycode);
-	f (keycode == 65307)
-		return (close_window(p));
+	if (keycode == 65307)
+		close_window(p);
 	return (1);
 }
 
-int mouse(int button, int x, int y, void *params)
+int test(s_params *p)
 {
-	printf("button: %i\n", button);
-
-	s_params *p = (s_params *) params;
-	update_color(p);
-	return (0);	
+	printf("hello %p\n", p);
+	return (1);
 }
 
 int	main(void)
@@ -58,7 +55,10 @@ int	main(void)
 	params.win_ptr = mlx_new_window(params.mlx_ptr, 990, 300, "test");
 
 	mlx_key_hook(params.win_ptr, press, &params);
-	mlx_hook(params.win_ptr, 17, 1L<<3, &close_window, &params);
-	mlx_mouse_hook(params.win_ptr, mouse, &params);
+	// mlx_mouse_hook(par	ams.win_ptr, mouse, &params);
+	mlx_hook(params.win_ptr, 17, 1L<<3, &close_window, &params); // close window
+	mlx_hook(params.win_ptr, 25, 0, &test, &params); // close window	mlx_loop(params.mlx_ptr);
+	mlx_hook(params.win_ptr, 7, 0, &test, &params); // close window	mlx_loop(params.mlx_ptr);
+	mlx_hook(params.win_ptr, 22, 0, &test, &params); // close window	mlx_loop(params.mlx_ptr);
 	mlx_loop(params.mlx_ptr);
 }
